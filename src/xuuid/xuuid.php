d63811e9ffc3a3ba6d9adb1d69610614f2f9da1b
luopan <luopan@w1.dev.aly.mararun.cn>
2016-12-11 14:26:04 +0800

    close persisten connect

--- src/xuuid/xuuid.php
+++ src/xuuid/xuuid.php
@@ -41,7 +41,7 @@ class Xuuid
             $ins     = new XuuidServer();
             foreach($confs as $conf)
             {
-                $ins->addServer($conf['host'],$conf['port']);
+                $ins->addServer($conf['host'],$conf['port'],false);
             }
 
         }
